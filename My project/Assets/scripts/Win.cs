fa2b358bfeb4ed5bb7f1702cc332595cee8b6785
tiaguii17 <103074957+tiaguii17@users.noreply.github.com>
2022-11-19 16:37:50 -0300

    V 0.0.6
    win sistem

---
+++ My project/Assets/scripts/Win.cs
@@ -0,0 +1,15 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.transform.gameObject.name == "Player")
+        {
+       
+            SceneManager.LoadScene(0);
+        }
+    }
+}
